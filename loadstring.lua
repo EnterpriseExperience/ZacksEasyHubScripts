1f3796f576c5048fecb7df09848e403e450b25e4
EnterpriseExperience <165463731+EnterpriseExperience@users.noreply.github.com>
2024-08-31 10:53:47 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/EnterpriseExperience/ZacksEasyHubScripts/main/source.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EnterpriseExperience/ZacksEasyHubScripts/main/script.lua"))()
